6a733379d6f39ac406cd5de9cb0471ea5ef26688
Роман Попов <rpopov@jinr.ru>
2021-10-15 10:18:22 +0300

    Init project

---
+++ main.py
@@ -0,0 +1,11 @@
+from spk_307 import *
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
