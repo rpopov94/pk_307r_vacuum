f3a560af896f7414af77322d32c243340fd88600
Роман Попов <rpopov@jinr.ru>
2021-11-08 14:52:14 +0300

    log for work

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from PyQt5.QtWidgets import QMessageBox
 from spk_307 import Ui_Dialog
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
@@ -5,6 +6,24 @@ from pymodbus.client.sync import ModbusTcpClient
 from settings import ip
 from utilites import Utilites
 import datetime
+from settings_ui import *
+import logging
+
+
+class QTextEditLogger(logging.Handler, QtCore.QObject):
+    appendPlainText = QtCore.pyqtSignal(str)
+
+    def __init__(self, parent):
+        super().__init__()
+        QtCore.QObject.__init__(self)
+        self.widget = QtWidgets.QPlainTextEdit(parent)
+        self.widget.setGeometry(QtCore.QRect(0, 0, 300, 100))
+        self.widget.setReadOnly(True)
+        self.appendPlainText.connect(self.widget.appendPlainText)
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.appendPlainText.emit(msg)
 
 
 class BrowserHandler(QtCore.QObject):
@@ -17,12 +36,10 @@ class BrowserHandler(QtCore.QObject):
 
 
 class SPK(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self, parent)
+    def __init__(self):
+        super().__init__()
         self.w_root = Ui_Dialog()
         self.w_root.setupUi(self)
-        self.client = ModbusTcpClient(ip)
-        self.client.connect()
         self.w_root.st_mption_1.clicked.connect(self.first_motion_control)
         self.w_root.st_motion_2.clicked.connect(self.second_motion_control)
         self.w_root.valve_11.clicked.connect(self.valve11_control)
@@ -32,22 +49,68 @@ class SPK(QtWidgets.QWidget):
         self.w_root.manage.clicked.connect(self.manage_control)
         self.w_root.stop.clicked.connect(self.stop_cotrol)
         self.w_root.settings.clicked.connect(self.settings_cotrol)
+        logTextBox = QTextEditLogger(self)
+        try:
+            self.client = ModbusTcpClient(ip)
+            self.client.connect()
+        except:
+            print('ModbusTcpClient connection error')
+          # log to text box
+        logTextBox.setFormatter(
+            logging.Formatter(
+                '%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s'))
+        logging.getLogger().addHandler(logTextBox)
+        logging.getLogger().setLevel(logging.DEBUG)
 
-        self.thread = QtCore.QThread()
-        self.browserHandler = BrowserHandler()
-        self.browserHandler.moveToThread(self.thread)
-        self.browserHandler.newTextAndColor.connect(self.moitor)
-        self.thread.started.connect(self.browserHandler.run)
-        self.thread.start()
+        # log to file
+        fh = logging.FileHandler('my-log.log')
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(
+            logging.Formatter(
+                '%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s'))
+        logging.getLogger().addHandler(fh)
 
+        # self.thread = QtCore.QThread()
+        # self.browserHandler = BrowserHandler()
+        # self.browserHandler.moveToThread(self.thread)
+        # self.browserHandler.newTextAndColor.connect(self.monitor)
+        # self.thread.started.connect(self.browserHandler.run)
+        # self.thread.start()
 
-    def reset_color(self):
-        self.w_root.st_mption_1.setStyleSheet(f'background: red;')
-        self.w_root.st_motion_2.setStyleSheet(f'background: red;')
-        self.w_root.valve_11.setStyleSheet(f'background: red;')
-        self.w_root.valve_12.setStyleSheet(f'background: red;')
-        self.w_root.valve_21.setStyleSheet(f'background: red;')
-        self.w_root.valve_22.setStyleSheet(f'background: red;')
+    def create_logger(path, widget: QtWidgets.QTextEdit):
+        log = logging.getLogger('main')
+        log.setLevel(logging.DEBUG)
+
+        file_formatter = logging.Formatter(
+            ('#%(levelname)-s, %(pathname)s, line %(lineno)d, [%(asctime)s]: '
+             '%(message)s'), datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        console_formatter = logging.Formatter(('#%(levelname)-s, %(pathname)s, '
+                                               'line %(lineno)d: %(message)s'))
+
+        log_window_formatter = logging.Formatter(
+            '#%(levelname)-s, %(message)s\n'
+        )
+
+        file_handler = logging.FileHandler(path)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(file_formatter)
+
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(console_formatter)
+        console_handler.setLevel(logging.DEBUG)
+        console_handler.setFormatter(console_formatter)
+
+        log_window_handler = logging.Handler()
+        log_window_handler.emit = lambda record: widget.insertPlainText(
+            log_window_handler.format(record)
+        )
+        log_window_handler.setLevel(logging.DEBUG)
+        log_window_handler.setFormatter(log_window_formatter)
+
+        log.addHandler(file_handler)
+        log.addHandler(console_handler)
+        log.addHandler(log_window_handler)
 
     def handle(self, address):
         cmd = False
@@ -71,17 +134,14 @@ class SPK(QtWidgets.QWidget):
     def second_motion_control(self):
         self.handle(261)
 
-
     def valve11_control(self):
         self.handle(280)
-
 
     def valve12_control(self):
         self.handle(281)
 
     def valve21_control(self):
         self.handle(282)
-
 
     def valve22_control(self):
         self.handle(283)
@@ -96,35 +156,44 @@ class SPK(QtWidgets.QWidget):
     def stop_cotrol(self):
         self.client.write_coil(258, value=0)
         self.client.write_register(330, value=0)
-        self.client.close()
 
     @QtCore.pyqtSlot()
-    def moitor(self):
-        now = datetime.datetime.now()
-        self.w_root.date.setText(now.strftime('%c'))
-        self.w_root.pr_1.setText(self.get_pressure(286))
-        self.w_root.pr_2.setText(self.get_pressure(272))
-        self.w_root.pr_3.setText(self.get_pressure(300))
-        self.w_root.pr_4.setText(self.get_pressure(314))
-        manage = Utilites.color(self.client.read_coils(258).bits[0])
-        self.w_root.manage.setStyleSheet(f'background: {manage};')
-        # word = Utilites.text_manage_control(manage)
-        # self.w_root.manage.setText(word)
-        c1 = Utilites.color(self.client.read_coils(260).bits[0])
-        self.w_root.st_mption_1.setStyleSheet(f'background: {c1};')
-        c2 = Utilites.color(self.client.read_coils(261).bits[0])
-        self.w_root.st_motion_2.setStyleSheet(f'background: {c2};')
-        c3 = Utilites.color(self.client.read_coils(290).bits[0])
-        self.w_root.valve_11.setStyleSheet(f'background: {c3};')
-        c4 = Utilites.color(self.client.read_coils(291).bits[0])
-        self.w_root.valve_12.setStyleSheet(f'background: {c4};')
-        c5 = Utilites.color(self.client.read_coils(292).bits[0])
-        self.w_root.valve_21.setStyleSheet(f'background: {c5};')
-        c6 = Utilites.color(self.client.read_coils(293).bits[0])
-        self.w_root.valve_22.setStyleSheet(f'background: {c6};')
+    def monitor(self):
+        try:
+            now = datetime.datetime.now()
+            self.w_root.date.setText(now.strftime('%c'))
+            self.w_root.pr_1.setText(self.get_pressure(286))
+            self.w_root.pr_2.setText(self.get_pressure(272))
+            self.w_root.pr_3.setText(self.get_pressure(300))
+            self.w_root.pr_4.setText(self.get_pressure(314))
+            manage = Utilites.color(self.client.read_coils(258).bits[0])
+            self.w_root.manage.setStyleSheet(f'background: {manage};')
+            c1 = Utilites.color(self.client.read_coils(260).bits[0])
+            self.w_root.st_mption_1.setStyleSheet(f'background: {c1};')
+            c2 = Utilites.color(self.client.read_coils(261).bits[0])
+            self.w_root.st_motion_2.setStyleSheet(f'background: {c2};')
+            c3 = Utilites.color(self.client.read_coils(290).bits[0])
+            self.w_root.valve_11.setStyleSheet(f'background: {c3};')
+            c4 = Utilites.color(self.client.read_coils(291).bits[0])
+            self.w_root.valve_12.setStyleSheet(f'background: {c4};')
+            c5 = Utilites.color(self.client.read_coils(292).bits[0])
+            self.w_root.valve_21.setStyleSheet(f'background: {c5};')
+            c6 = Utilites.color(self.client.read_coils(293).bits[0])
+            self.w_root.valve_22.setStyleSheet(f'background: {c6};')
+        except:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Error")
+            msg.setInformativeText('More information')
+            msg.setWindowTitle("Error")
+            msg.exec_()
 
     def settings_cotrol(self):
-        print('settings_cotrol')
+        self.dialog = QtWidgets.QMainWindow()
+        self.ui = Ui_Settings()
+        self.ui.setupUi(self.dialog)
+        self.dialog.show()
+        # logging.info('Запущено окно настроек')
 
 
 if __name__ == "__main__":
--- settings_ui.py
+++ settings_ui.py
@@ -1,9 +1,67 @@
 # -*- coding: utf-8 -*-
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from pymodbus.client.sync import ModbusTcpClient
+from settings import ip
+import logging
+
+
+# class Worker(QtCore.QThread):
+#     try:
+#         client = ModbusTcpClient(ip)
+#         client.connect()
+#         pump_1 = client.read_holding_registers(10300)
+#         pump_2 = client.read_holding_registers(10302)
+#         valve_1_max = client.read_holding_registers(10316)
+#         valve_2_max = client.read_holding_registers(10318)
+#         valve_3_max = client.read_holding_registers(10320)
+#         valve_4_max = client.read_holding_registers(10322)
+#         valve_1_min = client.read_holding_registers(10308)
+#         valve_2_min = client.read_holding_registers(10310)
+#         valve_3_min = client.read_holding_registers(10312)
+#         valve_4_min = client.read_holding_registers(10314)
+#     except:
+#         print('ModbusTcpClient connection error')
+#
+#     def __init__(self):
+#         QtCore.QThread.__init__(self)
+
+    # def run(self):
+    #     while True:
+    #         try:
+    #             pump_1 = self.client.read_holding_registers(10300)
+    #             pump_2 = self.client.read_holding_registers(10302)
+    #             valve_1_max = self.client.read_holding_registers(10316)
+    #             valve_2_max = self.client.read_holding_registers(10318)
+    #             valve_3_max = self.client.read_holding_registers(10320)
+    #             valve_4_max = self.client.read_holding_registers(10322)
+    #             valve_1_min = self.client.read_holding_registers(10308)
+    #             valve_2_min = self.client.read_holding_registers(10310)
+    #             valve_3_min = self.client.read_holding_registers(10312)
+    #             valve_4_min = self.client.read_holding_registers(10314)
+    #         except:
+    #             pump_1 = self.pump_1
+    #             pump_2 = self.pump_2
+    #             valve_1_max = self.valve_1_max
+    #             valve_2_max = self.valve_2_max
+    #             valve_3_max = self.valve_3_max
+    #             valve_4_max = self.valve_4_max
+    #             valve_1_min = self.valve_1_min
+    #             valve_2_min = self.valve_2_min
+    #             valve_3_min = self.valve_3_min
+    #             valve_4_min = self.valve_4_min
+    #         QtCore.QThread.msleep(1000)
+class SettingsHandler(QtCore.QObject):
+    newTextAndColor = QtCore.pyqtSignal()
+
+    def run(self):
+        while True:
+            self.newTextAndColor.emit()
+            QtCore.QThread.msleep(1000)
 
 
 class Ui_Settings(object):
+
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(675, 458)
@@ -74,17 +132,91 @@ class Ui_Settings(object):
         self.i_o_manage = QtWidgets.QPushButton(self.gridLayoutWidget)
         self.i_o_manage.setObjectName("i_o_manage")
         self.gridLayout.addWidget(self.i_o_manage, 3, 5, 1, 1)
+        self.send = QtWidgets.QPushButton(Dialog)
+        self.send.setGeometry(QtCore.QRect(580, 400, 75, 23))
+        self.send.setText("OK")
+        self.send.setObjectName("send")
+        try:
+            self.client = ModbusTcpClient(ip)
+            self.client.connect()
+        except:
+            print('ModbusTcpClient connection error')
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.ip_adress.clicked.connect(self.ip_address_control)
+        self.datchicki.clicked.connect(self.datchicki_control)
+        self.i_o_manage.clicked.connect(self.hand_manage)
+        self.send.clicked.connect(self.send_settings)
+
+        # self.thread = QtCore.QThread()
+        # self.browserHandler = SettingsHandler()
+        # self.browserHandler.moveToThread(self.thread)
+        # self.browserHandler.newTextAndColor.connect(self.datchicki_control)
+        # self.thread.started.connect(self.browserHandler.run)
+        # self.thread.start()
+
+    def ip_address_control(self):
+        print('ip_adress')
+
+    def datchicki_control(self):
+        try:
+            self.client.read_holding_registers(10300)
+            self.client.read_holding_registers(10302)
+            self.client.read_holding_registers(10316)
+            self.client.read_holding_registers(10318)
+            self.client.read_holding_registers(10320)
+            self.client.read_holding_registers(10322)
+            self.client.read_holding_registers(10308)
+            self.client.read_holding_registers(10310)
+            self.client.read_holding_registers(10312)
+            self.client.read_holding_registers(10314)
+        except:
+            pass
+
+    def hand_manage(self):
+        print('hand_manage')
+
+    def send_settings(self):
+        try:
+            self.client.write_registers(10300, int(self.valve_1.toPlainText()))
+            self.client.write_registers(10302, int(self.valve_2.toPlainText()))
+            self.client.write_registers(10316, int(self.klapan_1_min.toPlainText()))
+            self.client.write_registers(10318, int(self.klapan_2_min.toPlainText()))
+            self.client.write_registers(10320, int(self.klapan_3_min.toPlainText()))
+            self.client.write_registers(10322, int(self.klapan_4_min.toPlainText()))
+            self.client.write_registers(10308, int(self.klapan_1_max.toPlainText()))
+            self.client.write_registers(10310, int(self.klapan_2_max.toPlainText()))
+            self.client.write_registers(10312, int(self.klapan_3_max.toPlainText()))
+            self.client.write_registers(10314, int(self.klapan_4_max.toPlainText()))
+        except:
+            self.valve_1.setText('-1')
+            self.valve_2.setText('-1')
+            self.klapan_1_min.setText('-1')
+            self.klapan_2_min.setText('-1')
+            self.klapan_3_min.setText('-1')
+            self.klapan_4_min.setText('-1')
+            self.klapan_1_max.setText('-1')
+            self.klapan_2_max.setText('-1')
+            self.klapan_3_max.setText('-1')
+            self.klapan_4_max.setText('-1')
+            logging.error('Ошибка отправки данных, проверьте свой ввод!')
+
+    def update_values(self):
+        print('update_values')
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        Dialog.setWindowTitle(_translate("Dialog", "Settings"))
         self.label.setText(_translate("Dialog", "Min"))
         self.label_3.setText(_translate("Dialog", "Насос 1"))
         self.label_4.setText(_translate("Dialog", "Насос 2"))
         self.label_2.setText(_translate("Dialog", "Max"))
         self.ip_adress.setText(_translate("Dialog", "IP адрес"))
         self.datchicki.setText(_translate("Dialog", "Настройка датчиков"))
-        self.i_o_manage.setText(_translate("Dialog", "Ручное управление Вход-Выход"))
+        self.i_o_manage.setText(_translate("Dialog", "Ручное управление \n Вход-Выход"))
+
+
+
+
+
